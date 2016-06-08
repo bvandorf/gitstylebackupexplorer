72d5960a526e4dcb5c412212b9050a9f042b6646
bvandorf <bvandorf@gmail.com>
2016-06-07 22:17:17 -0500

    initial code

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace gitstylebackupexplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openBackupDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 24);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(491, 401);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem,
+            this.restoreToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(114, 48);
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.infoToolStripMenuItem.Text = "Info";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // restoreToolStripMenuItem
+            // 
+            this.restoreToolStripMenuItem.Name = "restoreToolStripMenuItem";
+            this.restoreToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.restoreToolStripMenuItem.Text = "Restore";
+            this.restoreToolStripMenuItem.Click += new System.EventHandler(this.restoreToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(491, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openBackupDBToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openBackupDBToolStripMenuItem
+            // 
+            this.openBackupDBToolStripMenuItem.Name = "openBackupDBToolStripMenuItem";
+            this.openBackupDBToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.openBackupDBToolStripMenuItem.Text = "Open Backup DB";
+            this.openBackupDBToolStripMenuItem.Click += new System.EventHandler(this.openBackupDBToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 425);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Git Style Backup Explorer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openBackupDBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restoreToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gitstylebackupexplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openBackupDBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //open backup db
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Backup DB File (*.db) | *.db;";
+            openFile.Multiselect = false;
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                PopulateTree(openFile.FileName);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //exit
+            Application.Exit();
+        }
+
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //info
+            if (LastNodeClick != null)
+            {
+                TreeNode node = LastNodeClick;
+
+                string nodeTag = node.Tag.ToString();
+                string nodeVersion = nodeTag.Substring(0, nodeTag.IndexOf("~"));
+                string nodeDirPath = nodeTag.Substring(nodeTag.IndexOf("~") + 1);
+                string nodeFileName = "";
+                if (nodeTag.Split('~').Length == 3)
+                {
+                    nodeFileName = nodeDirPath.Substring(nodeDirPath.IndexOf("~") + 1);
+                    nodeDirPath = nodeDirPath.Substring(0, nodeDirPath.IndexOf("~"));
+                }
+
+                if (nodeFileName != "")
+                {
+                    //file
+                    string sinfo = "";
+                    sinfo += "Name: " + db.Version[nodeVersion].File[nodeDirPath + "/" + nodeFileName].Name + "\n";
+                    sinfo += "Directory: " + nodeDirPath + "\n";
+                    sinfo += "Version: " + db.Version[nodeVersion].Number.ToString() + "\n";
+                    sinfo += "Date: " + db.Version[nodeVersion].File[nodeDirPath + "/" + nodeFileName].Date.ToShortDateString() + " " + db.Version[nodeVersion].File[nodeDirPath + "/" + nodeFileName].Date.ToLongTimeString() + "\n";
+                    sinfo += "Date Modified: " + db.Version[nodeVersion].File[nodeDirPath + "/" + nodeFileName].DateModified.ToShortDateString() + " " + db.Version[nodeVersion].File[nodeDirPath + "/" + nodeFileName].DateModified.ToLongTimeString() + "\n";
+                    sinfo += "Size: " + db.Version[nodeVersion].File[nodeDirPath + "/" + nodeFileName].Size + " MB\n";
+                    sinfo += "Hash: " + HashToString(db.Version[nodeVersion].File[nodeDirPath + "/" + nodeFileName].Hash) + "\n";
+
+                    MessageBox.Show(sinfo, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    //dir
+                    string sinfo = "";
+                    sinfo += "Directory: " + nodeDirPath + "\n";
+                    sinfo += "Version: " + db.Version[nodeVersion].Number.ToString() + "\n";
+                    sinfo += "Date: " + db.Version[nodeVersion].Date.ToShortDateString() + " " + db.Version[nodeVersion].Date.ToLongTimeString() + "\n";
+                    sinfo += "Hash: " + HashToString(db.Version[nodeVersion].Hash) + "\n";
+
+                    MessageBox.Show(sinfo, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void restoreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //restore
+            if (LastNodeClick != null)
+            {
+                TreeNode node = LastNodeClick;
+
+                string nodeTag = node.Tag.ToString();
+                string nodeVersion = nodeTag.Substring(0, nodeTag.IndexOf("~"));
+                string nodeDirPath = nodeTag.Substring(nodeTag.IndexOf("~") + 1);
+                string nodeFileName = "";
+                if (nodeTag.Split('~').Length == 3)
+                {
+                    nodeFileName = nodeDirPath.Substring(nodeDirPath.IndexOf("~") + 1);
+                    nodeDirPath = nodeDirPath.Substring(0, nodeDirPath.IndexOf("~"));
+                }
+
+                System.IO.FileInfo fiConfigFileInfo = new System.IO.FileInfo(configFilePath);
+
+                if (nodeFileName != "")
+                {
+                    //file
+                    SaveFileDialog saveFile = new SaveFileDialog();
+                    saveFile.FileName = nodeFileName;
+                    saveFile.OverwritePrompt = true;
+                    if (saveFile.ShowDialog() == DialogResult.OK)
+                    {
+                        string StatusMessage = "Done";
+                        bool ErrorStatus = false;
+                        foreach (string key in db.Version[nodeVersion].File.Keys)
+                        {
+                            if (key == nodeDirPath + "/" + nodeFileName)
+                            {
+                                string sourceFile = fiConfigFileInfo.Directory.FullName + "\\" + HashToString(db.Version[nodeVersion].File[key].Hash);
+                                string destFile = saveFile.FileName;
+
+                                try
+                                {
+                                    System.IO.File.Copy(sourceFile, destFile, true);
+                                }
+                                catch (Exception ex)
+                                {
+                                    StatusMessage = "File Restore Did Not Complete\n" + ex.ToString();
+                                    ErrorStatus = true;
+                                }
+                            }
+                        }
+                        if (ErrorStatus)
+                            MessageBox.Show(StatusMessage, "Restore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        else
+                            MessageBox.Show(StatusMessage, "Restore", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    //dir
+                    FolderBrowserDialog saveFolder = new FolderBrowserDialog();
+                    if (saveFolder.ShowDialog() == DialogResult.OK)
+                    {
+                        string StatusMessage = "Done";
+                        bool ErrorStatus = false;
+                        foreach (string key in db.Version[nodeVersion].File.Keys)
+                        {
+                            if (key.StartsWith(nodeDirPath))
+                            {
+                                string sourceFile = fiConfigFileInfo.Directory.FullName + "\\" + HashToString(db.Version[nodeVersion].File[key].Hash);
+                                string destFile = key.Replace("/", "\\").Replace(nodeDirPath, saveFolder.SelectedPath);
+
+                                try
+                                {
+                                    System.IO.FileInfo destFileInfo = new System.IO.FileInfo(destFile);
+                                    if (!destFileInfo.Directory.Exists)
+                                        System.IO.Directory.CreateDirectory(destFileInfo.Directory.FullName);
+
+                                    System.IO.File.Copy(sourceFile, destFile);
+                                }
+                                catch (Exception ex)
+                                {
+                                    StatusMessage = "File Restore Did Not Complete\n" + ex.ToString();
+                                    ErrorStatus = true;
+                                }
+                            }
+                        }
+                        if (ErrorStatus)
+                            MessageBox.Show(StatusMessage, "Restore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        else
+                            MessageBox.Show(StatusMessage, "Restore", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+        private string HashToString(byte[] hash)
+        {
+            string shash = "";
+            foreach(byte b in hash)
+            {
+                shash += b.ToString("000");
+            }
+            return shash;
+        }
+
+        class bdb
+        {
+            public bool Inuse;
+            public Dictionary<string, bdb_version> Version = new Dictionary<string, bdb_version>();
+        }
+
+        class bdb_version
+        {
+            public int Number;
+            public Dictionary<string, bdb_version_file> File = new Dictionary<string, bdb_version_file>();
+            public byte[] Hash;
+            public DateTime Date = new DateTime();
+        }
+
+        class bdb_version_file
+        {
+            public string Name;
+            public byte[] Hash;
+            public DateTime Date;
+            public DateTime DateModified;
+            public string Size;
+        }
+
+        bdb db = new bdb();
+        string configFilePath = "";
+        private void PopulateTree(string cfgFile)
+        {
+            try
+            {
+                configFilePath = cfgFile;
+                LastNodeClick = null;
+
+                db = Newtonsoft.Json.JsonConvert.DeserializeObject<bdb>(System.IO.File.ReadAllText(cfgFile));
+
+                treeView1.Nodes.Clear();
+                //add tree item for each version
+                foreach(string key in db.Version.Keys)
+                {
+                    bdb_version ver = db.Version[key];
+
+                    TreeNode tn = new TreeNode(ver.Number.ToString() + " : " + ver.Date.ToShortDateString() + " " + ver.Date.ToLongTimeString());
+                    tn.Tag = ver.Number.ToString() + "~";
+                    tn.Nodes.Add("");
+                    treeView1.Nodes.Add(tn);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Opening Backup File", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode node = e.Node;
+            node.Nodes.Clear();
+
+            string nodeTag = node.Tag.ToString();
+            string nodeVersion = nodeTag.Substring(0, nodeTag.IndexOf("~"));
+            string nodeDirPath = nodeTag.Substring(nodeTag.IndexOf("~") + 1);
+            string nodeFileName = "";
+            if (nodeTag.Split('~').Length == 3)
+            {
+                nodeFileName = nodeDirPath.Substring(nodeDirPath.IndexOf("~") + 1);
+                nodeDirPath = nodeDirPath.Substring(0, nodeDirPath.IndexOf("~"));
+            }
+
+            string nodeKey = "";
+            TreeNode tn = node;
+            while(tn.Parent != null)
+            {
+                nodeKey = tn.Text + "\\" + nodeKey;
+                tn = tn.Parent;
+            }
+
+            //fill only the next row of items
+            bdb_version ver = db.Version[nodeVersion];
+
+            List<string> sortedKeys = new List<string>();
+            foreach(string key in ver.File.Keys)
+            {
+                sortedKeys.Add(key);
+            }
+            sortedKeys.Sort();
+
+            foreach(string sfile in sortedKeys)
+            {
+                if (sfile.StartsWith(nodeKey) || sfile.StartsWith(nodeKey.TrimEnd('\\')+"/"))
+                {
+                    string stmp = sfile;
+                    if (nodeKey != "")
+                    {
+                        stmp = sfile.Replace(nodeKey, "");
+                        stmp = stmp.Replace(nodeKey.TrimEnd('\\') + "/", "");
+                    }
+
+                    string[] split = stmp.Split(new char[] { '\\', '/' });
+                    if (node.Nodes.ContainsKey(split[0]) == false)
+                    {
+                        if (split.Length == 1)
+                        {
+                            //file
+                            node.Nodes.Add(new TreeNode() { Name = split[0], Text = split[0], Tag = nodeVersion + "~" + nodeKey.TrimEnd('\\') + "~" + split[0] });
+                        }
+                        else
+                        {
+                            //dir
+                            int inode = node.Nodes.Add(new TreeNode() { Name = split[0], Text = split[0], Tag = nodeVersion + "~" + nodeKey + split[0] });
+                            node.Nodes[inode].Nodes.Add("");
+                        }
+                    }
+                }
+            }
+        }
+
+        TreeNode LastNodeClick;
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            LastNodeClick = e.Node;
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (!e.Node.IsExpanded)
+                e.Node.Expand();
+        }
+    }
+}
